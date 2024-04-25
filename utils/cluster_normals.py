30543a1945a3933e8bca71a8a606d63fecff845a
ptellier <ptellier@student.ubc.ca>
2024-04-25 15:15:42 -0700

    cluster normals WIP

--- utils/cluster_normals.py
+++ utils/cluster_normals.py
@@ -2,26 +2,34 @@ from typing import Callable
 import numpy as np
 from numpy import ndarray
 import open3d.geometry as o3d_geom
-import surface_normals as sn
+from open3d.geometry import PointCloud
+from surface_normals import estimate_surface_normals
 
 
-class ClusterNormals():
+class ClusterNormals:
 
-    def __init__(self, pcd, clustering_algo: Callable[[ndarray], float]):
-        self.clustering_fn = clustering_algo
+    def __init__(self, pcd: PointCloud, clustering_fn: Callable[[ndarray], float]):
+        self.clustering_fn = clustering_fn
         self.pcd = pcd
-        self.tree = o3d_geom.KDTreeFlann(pcd)
-        self.normals = sn.estimate_surface_normals()
-    
+        self.kd_tree = o3d_geom.KDTreeFlann(pcd)
 
-    def find_knn_radius(self, anchor, radius: float):
-        [k, idx, _] = self.tree.search_radius_vector_3d(self.pcd.points[anchor], radius)
+        estimate_surface_normals(
+            self.pcd,
+            voxel_down_sample_size=0.01,
+            normal_estimation_radius=0.02,
+            orientation_ref=np.array([0.0, 0.0, 1.0]),
+            inplace=True
+        )
+
+    def find_knn_radius(self, anchor: int, radius: float):
+        num_points, idx, coordinates = self.kd_tree.search_radius_vector_3d(query=self.pcd.points[anchor], radius=radius)
         return idx
 
-    #def cluster_normals():
-
-        # for each point cloud data we have
-        #    find the points with a radius using knn_radius
-        #    find normals of those points using estimate_surface_normals
-        #    cluster these normals
-        # endfor
+    def cluster_normals(self):
+        """
+        for each point cloud point we have:
+            find the points with a radius using knn_radius
+            find normals of those points using estimate_surface_normals
+            cluster these normals
+        """
+        pass
