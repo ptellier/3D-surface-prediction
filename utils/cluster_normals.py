e7843ace1cfc380607202f8c4756c1e92db3953c
Akshat Poddar <akshatpoddar312002@gmail.com>
2024-04-25 13:38:37 -0700

    Added cluster_normals

---
+++ utils/cluster_normals.py
@@ -0,0 +1,23 @@
+import numpy as np
+import open3d.geometry as o3d_geom
+import surface_normals as sn
+
+
+class ClusterNormals():
+
+    def __init__(self, pcd):
+        self.tree = o3d_geom.KDTreeFlann(pcd)
+        self.normals = sn.estimate_surface_normals()
+    
+
+    def find_knn_radius(self, pcd: np.ndarray, anchor, radius: float):
+        [k, idx, _] = self.tree.search_radius_vector_3d(pcd.points[anchor], radius)
+        return pcd[idx[1:], :]
+
+    #def cluster_normals():
+
+        # for each point cloud data we have
+        #    find the points with a radius using knn_radius
+        #    find normals of those points using estimate_surface_normals
+        #    cluster these normals
+        # endfor
---
+++ utils/visualizer.py
@@ -0,0 +1,8 @@
+import open3d.geometry as o3d_geom
+import numpy as np
+
+
+
+def visualize_pc(pc: np.ndarray, pc2: np.ndarray, color: np.ndarray):
+    return NotImplementedError
+
