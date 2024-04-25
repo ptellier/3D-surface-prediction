c3ed6ab91e28b06eb627d3523356a8ff3d035101
ptellier <ptellier@student.ubc.ca>
2024-04-25 16:22:05 -0700

    cluster normals WIP

--- utils/cluster_normals.py
+++ utils/cluster_normals.py
@@ -3,26 +3,33 @@ import numpy as np
 from numpy import ndarray
 import open3d.geometry as o3d_geom
 from open3d.geometry import PointCloud
-from surface_normals import estimate_surface_normals
+from utils.surface_normals import estimate_surface_normals
 
 
 class ClusterNormals:
 
-    def __init__(self, pcd: PointCloud, clustering_fn: Callable[[ndarray], float]):
-        self.clustering_fn = clustering_fn
-        self.pcd = pcd
-        self.kd_tree = o3d_geom.KDTreeFlann(pcd)
+    def __init__(self,
+                 pcd: PointCloud,
+                 clustering_fn: Callable[[ndarray], float],
+                 normal_estimation_radius,
+                 voxel_down_sample_size=None,
+                 orientation_ref=np.array([0.0, 0.0, 1.0])
+                 ):
+        self._clustering_fn = clustering_fn
+        self._pcd = pcd
+        self._kd_tree = o3d_geom.KDTreeFlann(pcd)
 
         estimate_surface_normals(
-            self.pcd,
-            voxel_down_sample_size=0.01,
-            normal_estimation_radius=0.02,
-            orientation_ref=np.array([0.0, 0.0, 1.0]),
+            self._pcd,
+            voxel_down_sample_size=voxel_down_sample_size,
+            normal_estimation_radius=normal_estimation_radius,
+            orientation_ref=orientation_ref,
             inplace=True
         )
 
     def find_knn_radius(self, anchor: int, radius: float):
-        num_points, idx, coordinates = self.kd_tree.search_radius_vector_3d(query=self.pcd.points[anchor], radius=radius)
+        num_points, idx, coordinates = self._kd_tree.search_radius_vector_3d(query=self._pcd.points[anchor],
+                                                                             radius=radius)
         return idx
 
     def cluster_normals(self):
@@ -33,3 +40,7 @@ class ClusterNormals:
             cluster these normals
         """
         pass
+
+    @property
+    def pcd(self):
+        return self._pcd
